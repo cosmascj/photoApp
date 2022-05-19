cd7cbe4f3c542745320aacb6d6dced30acec8edf
Cosmas Egbosi <cosmascj@gmail.com>
2022-05-19 20:37:02 +0100

    Revert "bug-fix"

--- app/src/main/jni/Keys.c
+++ app/src/main/jni/Keys.c
@@ -1,8 +1,5 @@
 #include <jni.h>
 JNIEXPORT jstring JNICALL
 Java_cosmas_egbosi_photosapp_MainActivity_getPixaApiKey(JNIEnv *env, jobject instance) {
-
- return (*env)->  NewStringUTF(env, "XXXXXXX");
-
-
+ return (*env)->  NewStringUTF(env, "2*********");
 }
